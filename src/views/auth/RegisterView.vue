c030841dcb31f3ec71a929a6b9ee83384b6bf11e
danielleirishmaymartinez <danielleirishmaymartinez@gmail.com>
2024-10-22 13:06:11 +0800

    feat:added login and register page

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -30,8 +30,8 @@ function onClick() {
             <v-col cols="12" md="6" class="mx-auto pt-16">
               <v-card
                 class="mx-auto"
-                prepend-icon="mdi-account"
-                subtitle="Login Form"
+                prepend-icon="mdi-account-plus"
+                subtitle="Register Form"
                 image="/images/bg-favicon.jpg"
                 elevation="24"
               >
@@ -41,6 +41,16 @@ function onClick() {
 
                 <v-card-text class="bg-surface-light pt-4">
                   <v-form fast-fail @submit.prevent>
+                    <v-text-field
+                      label="Firstname:"
+                      variant="outlined"
+                    ></v-text-field>
+
+                    <v-text-field
+                      label="Lastname:"
+                      variant="outlined"
+                    ></v-text-field>
+
                     <v-text-field
                       label="Email:"
                       variant="outlined"
@@ -52,15 +62,21 @@ function onClick() {
                       variant="outlined"
                     ></v-text-field>
 
+                    <v-text-field
+                      label="Password Confirmation:"
+                      type="password"
+                      variant="outlined"
+                    ></v-text-field>
+
                     <v-btn class="mt-2" type="submit" block>Submit</v-btn>
                   </v-form>
 
                   <v-divider class="my-5"></v-divider>
 
                   <h5 class="text-center">
-                    Don't have account?
-                    <RouterLink class="text-primary" to="/register"
-                      >Click here to Register</RouterLink
+                    Already have account?
+                    <RouterLink class="text-primary" to="/">
+                      Click here to Login</RouterLink
                     >
                   </h5>
                 </v-card-text>
