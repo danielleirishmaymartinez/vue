768f3bdb6a6e81581a05f3cf0fd8f13838894a6b
unknown <vtiu635@gmail.com>
2024-11-02 03:39:40 +0800

    feat:added ProfileView, SavedView, SettingsView and updated the routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import LandingView from '@/views/system/LandingView.vue'
 import HomepageView from '@/views/system/HomepageView.vue'
+import ProfileView from '@/views/system/ProfileView.vue'
+import SettingsView from '@/views/system/SettingsView.vue'
+import SavedView from '@/views/system/SavedView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +29,21 @@ const router = createRouter({
       path: '/homepage',
       name: 'homepage',
       component: HomepageView,
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView,
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: SettingsView,
+    },
+    {
+      path: '/saved',
+      name: 'saved',
+      component: SavedView,
     },
   ],
 })
---
+++ src/views/system/ProfileView.vue
@@ -0,0 +1,58 @@
+<script setup>
+import SidebarNav from '@/components/system/SidebarNav.vue';
+import { useDisplay } from 'vuetify';
+import { ref } from 'vue';
+import supabase from '@/utils/supabase.js';
+import { useRouter } from 'vue-router';
+
+const { mobile } = useDisplay();
+const drawerVisible = ref(!mobile.value);
+const router = useRouter();
+
+// Logout function
+async function logout() {
+  try {
+    await supabase.auth.signOut();
+    router.push('/login');
+  } catch (error) {
+    console.error('Logout error:', error);
+  }
+}
+</script>
+
+<template>
+  <v-responsive class="border rounded">
+    <v-app>
+      <!-- Top Navbar -->
+      <v-app-bar app elevation="0" color="transparent" class="px-4">
+        <v-btn icon @click="drawerVisible = !drawerVisible" v-if="mobile">
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+
+        <v-toolbar-title class="d-flex align-center" @click="$router.push('/homepage')">
+          <img src="/images/logo.png" alt="logo" class="mr-2" width="50" />
+          <span class="ml-1">STASH</span>
+        </v-toolbar-title>
+
+        <v-text-field
+          solo
+          flat
+          dense
+          prepend-inner-icon="mdi-magnify"
+          label="Search"
+          hide-details
+          class="d-flex flex-column mt-20"
+        ></v-text-field>
+
+        <v-btn text @click="logout">Logout</v-btn>
+      </v-app-bar>
+
+      <v-container fluid class="d-flex">
+        <SidebarNav v-model:drawer="drawerVisible" />
+        <v-main class="mt-5 pt-3">
+
+        </v-main>
+      </v-container>
+    </v-app>
+  </v-responsive>
+</template>
