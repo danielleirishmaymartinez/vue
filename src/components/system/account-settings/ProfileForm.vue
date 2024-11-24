c8399cfadddf2022ec10aa4c228f8cdbbc832af0
PCCD MASTER <lesliemaepelletero@gmail.com>
2024-11-24 16:03:33 +0800

    feat: added authUser and helpers

---
+++ src/components/system/account-settings/ProfileForm.vue
@@ -0,0 +1,106 @@
+<script setup>
+import { requiredValidator, integerValidator } from '@/utils/validators';
+import AlertNotification from '@/components/common/AlertNotification.vue';
+import { formActionDefault } from '@/utils/supabase.js';
+import { useAuthUserStore } from '@/stores/authUser';
+import { ref } from 'vue';
+
+const authStore = useAuthUserStore();
+
+const formData = ref({
+  firstname: authStore.userData?.firstname || '',
+  middlename: authStore.userData?.middlename || '',
+  lastname: authStore.userData?.lastname || '',
+  email: authStore.userData?.email || '',
+  phone: authStore.userData?.phone || '',
+});
+
+const formAction = ref({ ...formActionDefault });
+const refVForm = ref();
+
+const onSubmit = async () => {
+  formAction.value = { ...formActionDefault, formProcess: true };
+
+  const { data, error } = await authStore.updateUserInformation(formData.value);
+
+  if (error) {
+    formAction.value.formErrorMessage = error.message;
+    formAction.value.formStatus = error.status;
+  } else if (data) {
+    formAction.value.formSuccessMessage = 'Successfully updated account.';
+  }
+
+  formAction.value.formProcess = false;
+};
+
+const onFormSubmit = () => {
+  refVForm.value?.validate().then(({ valid }) => {
+    if (valid) onSubmit();
+  });
+};
+</script>
+
+<template>
+  <AlertNotification
+    :form-success-message="formAction.formSuccessMessage"
+    :form-error-message="formAction.formErrorMessage"
+  />
+
+  <v-form ref="refVForm" @submit.prevent="onFormSubmit">
+    <v-row dense>
+      <v-col cols="12" sm="4">
+        <v-text-field
+          v-model="formData.firstname"
+          label="Firstname"
+          :rules="[requiredValidator]"
+        />
+      </v-col>
+
+      <v-col cols="12" sm="4">
+        <v-text-field
+          v-model="formData.middlename"
+          label="Middlename"
+        />
+      </v-col>
+
+      <v-col cols="12" sm="4">
+        <v-text-field
+          v-model="formData.lastname"
+          label="Lastname"
+          :rules="[requiredValidator]"
+        />
+      </v-col>
+
+      <v-col cols="12" sm="6">
+        <v-text-field
+          readonly
+          disabled
+          v-model="formData.email"
+          label="Email"
+          prepend-inner-icon="mdi-email-outline"
+        />
+      </v-col>
+
+      <v-col cols="12" sm="6">
+        <v-text-field
+          v-model="formData.phone"
+          label="Phone Number"
+          prepend-inner-icon="mdi-phone"
+          prefix="+63"
+          :rules="[requiredValidator, integerValidator]"
+        />
+      </v-col>
+    </v-row>
+
+    <v-btn
+      class="mt-2"
+      type="submit"
+      color="red-darken-4"
+      prepend-icon="mdi-account-box-edit-outline"
+      :disabled="formAction.formProcess"
+      :loading="formAction.formProcess"
+    >
+      Update Information
+    </v-btn>
+  </v-form>
+</template>
